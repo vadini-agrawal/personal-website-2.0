b1676694c4f4cc29cb1d2c411e3a3f991b0cd6ef
vagrawal48 <vadini@gatech.edu>
2020-12-05 15:58:23 -0600

    last ditch attempt at GA

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -65,5 +65,37 @@ module.exports = {
       },
     },
   },
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        process.env.GOOGLE_ANALYTICS_TRACKING, // Google Analytics / GA
+      ],
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      // The property ID; the tracking code won't be generated without it
+      trackingId: process.env.GOOGLE_ANALYTICS_TRACKING,
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Defers execution of google analytics script after page load
+      defer: false,
+    },
+  },
+  
   ],
 }
